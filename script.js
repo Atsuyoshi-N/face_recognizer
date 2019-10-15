04322540468ba6894a23b368c3ab982a80c4a58d
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-10-15 22:57:20 +0900

    Prepare server

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const video = document.getElementById('video');
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromDisk('./models'),
-  faceapi.nets.faceLandmark68Net.loadFromDisk('./models'),
-  faceapi.nets.faceRecognitionNet.loadFromDisk('./models'),
-  faceapi.nets.faceExpressionNet.loadFromDisk('./models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models'),
 ]).then(startVideo);
 
 function startVideo() {
@@ -33,5 +34,6 @@ video.addEventListener('play', () => {
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
   }, 100);
 });
